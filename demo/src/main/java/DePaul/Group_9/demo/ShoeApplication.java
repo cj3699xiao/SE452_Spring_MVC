4ff85e189d2216428f563a51d45195726b5f0abd
cj3699xiao <354123302@qq.com>
2020-05-01 23:36:31 -0500

    db_h2_mem_setup

--- demo/src/main/java/DePaul/Group_9/demo/ShoeApplication.java
+++ demo/src/main/java/DePaul/Group_9/demo/ShoeApplication.java
@@ -1,13 +1,34 @@
 package DePaul.Group_9.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import DePaul.Group_9.demo.model.Address;
+
 
 @SpringBootApplication
 public class ShoeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoeApplication.class, args);
+//		Address a = new Address();
+//		a.getState();		
 	}
 
+//	@Value("${userid}")
+//	private String userId;
+//	
+//	@Bean
+//	public CommandLineRunner demo() {
+//		return (args) ->{
+//			System.out.println("-----");
+//			System.out.println(userId);
+//			System.out.println("-----");
+//		};
+//		
+//	}
+	
 }
