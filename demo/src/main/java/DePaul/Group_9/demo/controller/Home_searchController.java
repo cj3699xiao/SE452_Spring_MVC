fee6fd8e50c4b6f8e7a1b282aa108ab2e10b67e6
cj3699xiao <354123302@qq.com>
2020-05-18 01:50:51 -0500

    5/18/2020

---
+++ demo/src/main/java/DePaul/Group_9/demo/controller/Home_searchController.java
@@ -0,0 +1,21 @@
+package DePaul.Group_9.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+@RequestMapping("/home_search")
+public class Home_searchController {
+  
+//    @GetMapping("/home_search")
+//    public String home_searchpage(Model model) {
+//        
+//        return "home_search";
+//    }
+       
+    
+}
--- demo/src/main/resources/static/js/custom.js
+++ demo/src/main/resources/static/js/custom.js
@@ -1,6 +1,6 @@
 firstTime = sessionStorage.getItem("firstTime");
 
 if (firstTime === null){
-    alert('Hello from SE352/452');
+    alert('Hello from SE452_Group9. Welcome to Online Shoe Shop');
     sessionStorage.setItem("firstTime", "false");
 }    
