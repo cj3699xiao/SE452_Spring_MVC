7200aad020ad924faab87229969151eb70b61b85
qawazswse <31367515+qawazswse@users.noreply.github.com>
2020-08-24 11:25:58 -0500

    Update SearchController.java

--- demo/src/main/java/DePaul/Group_9/demo/controller/SearchController.java
+++ demo/src/main/java/DePaul/Group_9/demo/controller/SearchController.java
@@ -24,4 +24,19 @@ public class SearchController {
 		return searchService.searchShoes(input);
 	}
 	
+	/*
+	 * 
+	 * 
+	 * 
+	 * 这个search方法要改许多东西：
+	 * 		1. 参照loginController中的customerLogin方法改url和参数
+	 * 		2. 参照request.login中的LoginRequest新建一个request类 （其中仅包含一个参数： private String input）
+	 * 		3. 选作：
+	 * 			1. 在model包中新建一个Shoes类（其中仅包含： brand， type， material， color， size， price 6 个成员变量）
+	 * 			2. 将 这个search方法中的返回值类型List<Shoes>中的Shoes定义为model中的Shoes而不是之前用的POJO中的Shoes
+	 * 			3. 用Postman测试一下
+	 * 
+	 * 
+	 */
+	
 }
